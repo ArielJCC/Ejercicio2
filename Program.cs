7a0783ddfcb18474bc1afb332c4cec7a2dacf5cc
unknown <xx06102002@gmail.com>
2022-11-20 16:19:26 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Crear un programa que lea un número e indique si es par o no 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+
+        Console.WriteLine("Digite un número");
+        num = int.Parse(Console.ReadLine());
+
+        if(num %2 == 0)
+        {
+            Console.WriteLine("El número es Par");
+        }
+        else
+        {
+            Console.WriteLine("el número no es Par (Impar)");
+        }
+    }
+}
